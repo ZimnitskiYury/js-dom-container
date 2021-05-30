f2f3978dd7a64caca13670fbd1e37c4ecec19567
Yury Zimnitski <demovie.inc@live.com>
2021-05-30 20:11:58 +0300

    Move methods from constructor

--- VanillaJQuery.js
+++ VanillaJQuery.js
@@ -1,59 +1,69 @@
 class VanillaJQuery {
   constructor(node) {
     this.node = node;
-    this.addClass = function addClass(className) {
-      this.node.classList.add(className);
-      return this;
-    };
-    this.removeClass = function removeClass(className) {
-      this.node.classList.remove(className);
-      return this;
-    };
-    this.append = function append(element) {
-      this.node.append(element);
-      return this;
-    };
-    this.remove = function remove() {
-      this.node.remove();
-    };
-    this.text = function text() {
-      return this.node.innerText;
-    };
-    this.attr = function attr(name, value) {
-      this.node.setAttribute(name, value);
-      return this;
-    };
-    this.children = function children() {
-      this.node = this.node.children;
-      return this;
-    };
-    this.empty = function empty() {
-      while (this.node.firstChild) {
-        this.node.removeChild(this.node.firstChild);
+  }
+
+  addClass(className) {
+    this.node.classList.add(className);
+    return this;
+  }
+
+  removeClass(className) {
+    this.node.classList.remove(className);
+    return this;
+  }
+
+  append(element) {
+    this.node.append(element);
+    return this;
+  }
+
+  remove() {
+    this.node.remove();
+  }
+
+  text() {
+    return this.node.innerText;
+  }
+
+  attr(name, value) {
+    this.node.setAttribute(name, value);
+    return this;
+  }
+
+  children() {
+    this.node = this.node.children;
+    return this;
+  }
+
+  empty() {
+    while (this.node.firstChild) {
+      this.node.removeChild(this.node.firstChild);
+    }
+    return this;
+  }
+
+  css(property, value) {
+    if (property instanceof Map) {
+      property.forEach((val, key) => {
+        this.node.style[key] = val;
+      });
+    } else {
+      if (value === undefined) {
+        const computedStyle = getComputedStyle(this.node);
+        return computedStyle[property];
       }
+      this.node.style[property] = value;
+    }
+    return this;
+  }
+
+  click(callback) {
+    if (typeof callback !== 'function') {
       return this;
-    };
-    this.css = function css(property, value) {
-      if (property instanceof Map) {
-        property.forEach((val, key) => {
-          this.node.style[key] = val;
-        });
-      } else {
-        if (value === undefined) {
-          const computedStyle = getComputedStyle(this.node);
-          return computedStyle[property];
-        }
-        this.node.style[property] = value;
-      }
-      return this;
-    };
-    this.click = function click(callback) {
-      if (typeof callback !== 'function') {
-        return this;
-      }
-      this.node.addEventListener('click', callback);
-      return this;
-    };
+    }
+    this.node.addEventListener('click', callback);
+    return this;
   }
 }
 
